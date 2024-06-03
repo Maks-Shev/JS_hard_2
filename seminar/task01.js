d8c491d05d680dfe887b55609e131721f61a63c8
Maksim Shevchenko <maks.shev_87@mail.ru>
2024-06-03 13:11:44 +0300

    seminar task01

--- seminar/task01.js
+++ seminar/task01.js
@@ -1,21 +1,46 @@
 class BankAccount {
     // Приватное свойство для хранения баланса
+    #balance = 0;
+
+     // constructor(initialBalance) Конструктор для инициализации начального баланса
+    constructor(initialBalance = 0) {
+        if (initialBalance < 0) {   
+            throw new Error("Отрицательный баланс");
+        }
+        this.#balance = initialBalance;
+    };
 
     // Геттер для поучения текущего баланса
+    get balance() {
+        return this.#balance;
+    };
 
-    // Метод для внесения дененг на счет
+    // Метод для внесения денег на счет
+    deposite(amount) {
+        if (amount < 0) {
+            throw new Error("Отрицательная сумма");
+        }
+        this.#balance += amount;
+    };
 
     // withdraw(amount) Метод для снятия денег со счета
-
-    // constructor(initialBalance) Конструктор для инициализации начального баланса
-}
+    withdraw(amount) {
+        if (amount < 0) {
+            throw new Error("Отрицательная сумма");
+        }
+        if (amount > this.#balance) {
+            throw new Error("Недостаточно средств");
+        }
+        this.#balance -= amount;
+    };
+};
 
 // Создаем новый банковский счет с начальным балансом 500
 let account = new BankAccount(500);
-console.log(account.balance); // Выводит 500
+console.log(`Вы внесли на счет: ${account.balance}`); // Выводит 500
 
 account.deposite(200);
-console.log(account.balance); // Выводит 700
+console.log(`Остаток на счете после внесения: ${account.balance}`); // Выводит 700
 
 account.withdraw(100);
-console.log(account.balance); // Выводит 600
+console.log(`Остаток на счете после снятия: ${account.balance}`); // Выводит 600
